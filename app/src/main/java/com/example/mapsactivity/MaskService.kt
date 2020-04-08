ab952e27500d30caa1a39fbe6bfa5a6f7bb3791f
develop-sell <brianpark.3@hanmail.net>
2020-04-08 20:17:29 +0900

    add : maskService, Model 클래스 생성

---
+++ app/src/main/java/com/example/mapsactivity/MaskService.kt
@@ -0,0 +1,19 @@
+package com.example.mapsactivity
+
+import android.telecom.Call
+
+class MaskService {
+    const val BASE_URL = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1"
+
+    interface  MaskService {
+        @GET("storesByGeo/json")
+                /**
+                 * REST 요청을 처리하기 위한 메서드
+                 * @param par QueryMap을 통해 질의한 쿼리문을 Map으로 부터 받는다.
+                 * @return Call<T> 콜백 인터페이스 반환, T는 주고 받을 데이터 구조
+                 * @QueryMap 어노테이션은 위치가 바뀌어도 동적으로 값을 받아올 수 있게 한다.
+                 */
+        fun getTop(@QueryMap par: Map<Double, Double>): Call<MovieListResponse>
+
+    }
+}
---
+++ app/src/main/java/com/example/mapsactivity/Model.kt
@@ -0,0 +1,11 @@
+package com.example.mapsactivity
+
+class Model (
+    val code : String,
+    val name : String,
+    val addr : String,
+    val type : String,
+    val lat : Double,
+    val Ing : Double
+
+)
