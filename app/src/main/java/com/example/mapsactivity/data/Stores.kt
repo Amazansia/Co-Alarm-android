d3f305cae193fe5cf9eaeb861a487ce6c48fcba5
2jjingni <2jjingni@gmail.com>
2020-04-11 18:14:53 +0900

    add : MaskApi 위치 핀 띄우기 성공

--- app/src/main/java/com/example/mapsactivity/data/Stores.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mapsactivity.data
-
-data class Stores(
-    val count: Int,
-    val page: Int,
-    val storeInfos: List<Store>,
-    val totalCount: Int,
-    val totalPages: Int
-)
--- app/src/main/java/com/example/mapsactivity/data/StoresByAddr.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mapsactivity.data
-
-data class StoresByAddr(
-    val count: Int,
-    val stores: List<Store>
-)
